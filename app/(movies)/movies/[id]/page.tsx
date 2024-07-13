4b888da14bdb654d198f6984fd9ee049bcc6682e
hikim <kimhyoim23@gmail.com>
2024-07-14 00:17:46 +0900

    'edit'

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,22 +1,29 @@
 import { Suspense } from "react"
-import MovieInfo from "../../../../components/movie-info"
+import MovieInfo, {getMovie} from "../../../../components/movie-info"
 import MovieVideos from "../../../../components/movie-videos"
+
+interface IParams {
+    params: {id:string}
+};
+
+export async function generateMetadata({params: {id}}:IParams) {
+    const movie = await getMovie(id);
+    return {
+        title:movie.title
+    }
+}
 
 export default async function MovieDetail({
     params: {id}
-}:{
-    params: {id:string}
-}) {
+}:IParams) {
     return (
         <div>
-            <h3>Movie Detail Page</h3>
             <Suspense fallback={<h1>Loading movie info</h1>}>
                 <MovieInfo id={id}></MovieInfo>
             </Suspense>
-            <h4>Videos</h4>
-            <Suspense fallback={<h1>Loading movie videos</h1>}>
+            {<Suspense fallback={<h1>Loading movie videos</h1>}>
                 <MovieVideos id={id}></MovieVideos>
-            </Suspense>
+            </Suspense>}
         </div>
     )
 };
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,13 +1,20 @@
 import { API_URL } from "../app/(home)/page";
+import styles from "../styles/movie-info.module.css";
 
-async function getMovie(id:string) {
-    console.log(`Fetching movies: ${Date.now()}`);
-    await new Promise((resolve) => setTimeout(resolve, 3000))
+export async function getMovie(id:string) {
     const response = await fetch(`${API_URL}/${id}`);
     return response.json();
 }
 
 export default async function MovieInfo({id}:{id:string}) {
-    const info = await getMovie(id);
-    return <h6>{JSON.stringify(info)}</h6>
+    const movie = await getMovie(id);
+    return <div className={styles.container}>
+        <img className={styles.poster} src={movie.poster_path}></img>
+        <div className={styles.info}>
+            <h1 className={styles.title}>{movie.title}</h1>
+            <h3>☆ {movie.vote_average.toFixed(1)}</h3>
+            <p>{movie.overview}</p>
+            <a href={movie.homepage} target={"_blank"}>Homepage &rarr;</a>
+        </div>
+    </div>
 };
